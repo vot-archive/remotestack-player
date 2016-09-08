2adbd29ea4f5b50415b6fb5b5f39f4329588e56f
Vot Z <vot@siliconfen.co>
2016-09-08 20:42:41 +0100

    Updated window menu

--- renderer/lib/windowMenu.js
+++ renderer/lib/windowMenu.js
@@ -1,33 +1,51 @@
 const electron = require('electron');
 const Menu = electron.Menu;
 const app = electron.app;
+const shell = electron.shell;
 
 function intialise() {
   var template = [
     {
-      label: "RemoteStack",
+      label: 'RemoteStack',
       submenu: [
-          { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+          { label: 'About Application', selector: 'orderFrontStandardAboutPanel:' },
           // { label: 'Preferences', click: function () {
           //     ipc.send('open-settings-window')
           //   }
           // },
-          { type: "separator" },
-          { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+          // {
+          //   label: 'Learn More',
+          //   click () { shell.openExternal('http://remotestack.com') }
+          // },
+          { type: 'separator' },
+          { label: 'Minimize', accelerator: 'CmdOrCtrl+M', role: 'minimize' },
+          { label: 'Close', accelerator: 'CmdOrCtrl+W', role: 'minimize' },
+          { label: 'Quit', accelerator: 'Command+Q', click: function() { app.quit(); }}
       ]
     },
     {
-      label: "Edit",
+      label: 'Edit',
       submenu: [
-          { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-          { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-          { type: "separator" },
-          { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-          { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-          { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-          { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+          { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+          { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+          { type: 'separator' },
+          { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+          { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+          { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+          { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
       ]
-    }
+    },
+    // {
+    //   label: 'Player',
+    //   submenu: [
+    //       { label: 'Previous track', accelerator: '<'},
+    //       { label: 'Next track', accelerator: '>'},
+    //       { label: 'Play/Pause', accelerator: 'Space'},
+    //       { type: 'separator' },
+    //       { label: 'Volume up', accelerator: ''},
+    //       { label: 'Volume down', accelerator: ''}
+    //   ]
+    // }
   ];
 
   const menu = Menu.buildFromTemplate(template);
